7629bc07f6cdbf75c1e67ce016b83e49cf3f9713
DevBigEazi <adesholatajudeen1@gmail.com>
2025-10-09 10:38:04 +0100

    frontend folder setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <main>
+      <div className='flex justify-center items-center'>
+        <h2 className="text-green-600">Hello CirclePot</h2>
+      </div>
+    </main>
+  )
+}
+
+export default App
