f0faacfd16fb99d3973d943818fdc0a4225fb1d5
DevBigEazi <adesholatajudeen1@gmail.com>
2025-10-19 16:27:14 +0100

    ops: configured thirdweb

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { ConnectButton } from "thirdweb/react"
+import { client } from "./thirdwebClient"
+
 function App() {
 
   return (
     <main>
       <div className='flex justify-center items-center'>
         <h2 className="text-green-600">Hello CirclePot</h2>
+        <ConnectButton client={client} />
       </div>
     </main>
   )
