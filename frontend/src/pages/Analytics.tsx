3fa409fd98460a38b600c8d1bd8afd196abb47ac
DevBigEazi <adesholatajudeen1@gmail.com>
2025-11-10 13:10:11 +0100

    feat: add Navbar for Analytics page

--- frontend/src/pages/Analytics.tsx
+++ frontend/src/pages/Analytics.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useUserProfile } from '../hooks/useUserProfile'
+import { client } from '../thirdwebClient'
+import { useNavigate } from 'react-router'
+import { useThemeColors } from '../hooks/useThemeColors'
+import NavBar from '../components/NavBar'
 
 const Analytics: React.FC = () => {
+  const navigate = useNavigate()
+  const colors = useThemeColors()
+
+  const { profile } = useUserProfile(client)
   return (
-    <div className='bg-primary h-full'>Analytics</div>
+    <>
+      <NavBar colors={colors} userName={profile?.username} fullName={profile?.fullName} onBack={() => navigate('/')} />
+
+        {/* main UI */}
+      <div className="min-h-screen" style={{ backgroundColor: colors.background }}>
+        <h1>Analytics</h1>
+      </div>
+    </>
   )
 }
 
