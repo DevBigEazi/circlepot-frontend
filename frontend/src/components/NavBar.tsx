620c03a2e10acc68f185501b2cb876b8d23f3838
DevBigEazi <adesholatajudeen1@gmail.com>
2025-11-10 12:13:02 +0100

    chore: rename TopNav to NavBar

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBar :React.FC = () => {
+  return (
+    <div className='bg-primary'>NavBar</div>
+  )
+}
+
+export default NavBar
--- frontend/src/components/TopNav.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TopNav :React.FC = () => {
-  return (
-    <div className='bg-primary'>TopNav</div>
-  )
-}
-
-export default TopNav
