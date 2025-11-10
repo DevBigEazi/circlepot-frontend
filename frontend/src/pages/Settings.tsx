4c37681c2a47222d00b160b43ad0f5e208fba70d
DevBigEazi <adesholatajudeen1@gmail.com>
2025-11-10 13:28:11 +0100

    feat: add Navbar for Settings page

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,8 +1,28 @@
+import { Settings2 } from 'lucide-react'
 import React from 'react'
+import NavBar from '../components/NavBar'
+import { useNavigate } from 'react-router'
+import { useThemeColors } from '../hooks/useThemeColors'
 
 const Settings: React.FC = () => {
+  const navigate = useNavigate()
+  const colors = useThemeColors()
+  
   return (
-    <div>Settings</div>
+   <>
+   <NavBar 
+        variant="minimal"
+        onBack={() => navigate(-1)}
+        title="Settings"
+        titleIcon={<Settings2 size={24} />}
+        colors={colors}
+      />
+
+        {/* main UI */}
+      <div className='min-h-screen' style={{ backgroundColor: colors.background }}>
+        <h1>Settings</h1>
+      </div>
+   </>
   )
 }
 
