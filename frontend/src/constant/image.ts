f1902e8d4b2efcc6120cd03556f183dc837f6317
DevBigEazi <adesholatajudeen1@gmail.com>
2025-11-27 09:18:12 +0100

    ops: clean up dashboard to prepare for complete implementations

--- frontend/src/constant/image.ts
+++
@@ -1,7 +0,0 @@
-import fullLogo from '../assets/images/full-logo.png';
-import cUSD from '../assets/images/cusd.svg';
-import chrome from '../assets/images/chrome.svg';
-import passkey from '../assets/images/passkey.svg';
-import email from '../assets/images/email.svg';
-
-export default { fullLogo, cUSD, chrome, passkey, email };
