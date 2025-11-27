dafe56e0021dc90f13530a1ebefefaf59b260bf7
DevBigEazi <adesholatajudeen1@gmail.com>
2025-11-27 09:08:16 +0100

    chore: rename constant to constants

---
+++ frontend/src/constants/image.ts
@@ -0,0 +1,7 @@
+import fullLogo from '../assets/images/full-logo.png';
+import cUSD from '../assets/images/cusd.svg';
+import chrome from '../assets/images/chrome.svg';
+import passkey from '../assets/images/passkey.svg';
+import email from '../assets/images/email.svg';
+
+export default { fullLogo, cUSD, chrome, passkey, email };
