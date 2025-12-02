ffe214c8d716b3ac8f0aad3fc3777661922d0514
DevBigEazi <adesholatajudeen1@gmail.com>
2025-12-02 13:50:09 +0100

    add reputation address

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,6 +6,8 @@ export const CUSD_ADDRESS = import.meta.env.VITE_CUSD_ADDRESS;
 export const PERSONAL_SAVINGS_ADDRESS = import.meta.env.VITE_PERSONAL_SAVINGS_ADDRESS;
 export const CIRCLE_SAVINGS_ADDRESS = import.meta.env.VITE_CIRCLE_SAVINGS_ADDRESS;
 export const USER_PROFILE_ADDRESS = import.meta.env.VITE_USER_PROFILE_ADDRESS;
+export const REPUTATION_ADDRESS = import.meta.env.VITE_REPUTATION_ADDRESS;
+
 
 // Subgraph Configuration
 export const SUBGRAPH_URL = import.meta.env.VITE_SUBGRAPH_URL;
